78f29115c8b306e6662f36392c2f5347e62ab893
base0 <vacapp@live.com>
2021-10-19 21:57:25 +0700

    Ed Flo George

--- ballot_simplified.sol
+++ ballot_simplified.sol
@@ -3,10 +3,18 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /** 
- * simplified from a Remix example
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
  */
 contract Ballot {
    
+    struct Voter {
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
+    }
+
     struct Proposal {
         // If you can limit the length to a certain number of bytes, 
         // always use one of bytes1 to bytes32 because they are much cheaper
@@ -14,17 +22,19 @@ contract Ballot {
         uint voteCount; // number of accumulated votes
     }
 
-    Proposal[] public proposals;
-
     address public chairperson;
+
+    mapping(address => Voter) public voters;
+
+    Proposal[] public proposals;
 
     /** 
      * @dev Create a new ballot to choose one of 'proposalNames'.
      * @param proposalNames names of proposals
-     *  ["0x4564000000000000000000000000000000000000000000000000000000000000","0x4672656400000000000000000000000000000000000000000000000000000000","0x47656f7267650000000000000000000000000000000000000000000000000000"]
      */
     constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
+        voters[chairperson].weight = 1;
 
         for (uint i = 0; i < proposalNames.length; i++) {
             // 'Proposal({...})' creates a temporary
@@ -36,15 +46,7 @@ contract Ballot {
             }));
         }
     }
-
-    struct Voter {
-        bool hasRight;
-        bool voted;  // if true, that person already voted
-        uint vote;   // index of the voted proposal
-    }
-
-    mapping(address => Voter) public voters;
-
+    
     /** 
      * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
      * @param voter address of voter
@@ -58,9 +60,38 @@ contract Ballot {
             !voters[voter].voted,
             "The voter already voted."
         );
-        voters[voter].hasRight = true;
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
     }
 
+    /**
+     * @dev Delegate your vote to the voter 'to'.
+     * @param to address to which vote is delegated
+     */
+    function delegate(address to) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted.");
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            // We found a loop in the delegation, not allowed.
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+        sender.voted = true;
+        sender.delegate = to;
+        Voter storage delegate_ = voters[to];
+        if (delegate_.voted) {
+            // If the delegate already voted,
+            // directly add to the number of votes
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            // If the delegate did not vote yet,
+            // add to her weight.
+            delegate_.weight += sender.weight;
+        }
+    }
 
     /**
      * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
@@ -68,7 +99,7 @@ contract Ballot {
      */
     function vote(uint proposal) public {
         Voter storage sender = voters[msg.sender];
-        require(sender.hasRight, "No right to vote.");
+        require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
         sender.vote = proposal;
@@ -76,7 +107,7 @@ contract Ballot {
         // If 'proposal' is out of the range of the array,
         // this will throw automatically and revert all
         // changes.
-        proposals[proposal].voteCount += 1;
+        proposals[proposal].voteCount += sender.weight;
     }
 
     /** 
