1f0f7ae27bb567735ef8a07bb5fc677b8b332627
base0 <vacapp@live.com>
2021-10-19 16:08:51 +0700

    Update ballot_simplified.sol

--- ballot_simplified.sol
+++ ballot_simplified.sol
@@ -104,4 +104,24 @@ contract Ballot {
     {
         winnerName_ = proposals[winningProposal()].name;
     }
+
+    //https://ethereum.stackexchange.com/a/59335
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while (i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+    
+    function winnerName2() public view
+            returns (string memory winnerName_)
+    {
+        winnerName_ = bytes32ToString(proposals[winningProposal()].name);
+    }
 }
