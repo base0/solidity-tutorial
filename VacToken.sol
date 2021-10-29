b68bc5f3e3635d6584088128d0708bbf2416cbe6
base0 <vacapp@live.com>
2021-10-29 07:08:32 +0700

    Create VacToken.sol

---
+++ VacToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract VacToken is ERC20 {
+    constructor() ERC20("Virtual Ancient Coin", "VAC") {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+    
+    function decimals() public pure override returns (uint8) {
+        return 8;
+    }
+}
